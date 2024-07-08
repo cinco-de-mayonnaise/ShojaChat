38c02a8ece2b43ba70bf4261982a5ec9a5659737
AhnafAbdullah_IUB <124304231+cinco-de-mayonnaise@users.noreply.github.com>
2024-07-08 19:24:49 +0600

    Nothing

--- src/main/java/com/abdullah/shojachat/HelloApplication.java
+++ src/main/java/com/abdullah/shojachat/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application
     @Override
     public void start(Stage stage) throws IOException
     {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(""));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("mainWindow.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
